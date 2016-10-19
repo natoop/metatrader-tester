9b98fed6f3110d920419201e36ce90ffe7e725a7
Artem Nazarov <Artem Nazarov>
2016-10-18 23:46:37 -0400

    Initial commit

---
+++ MQLibrary/Helper/Delegation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MQPanel.Helper
+{
+    class Delegation
+    {
+        public delegate void SetDealDelegateTemplate(OperationData price, OrderData order);
+        public static SetDealDelegateTemplate SetDealDelegates;
+
+        public delegate void SetTradeDelegateTemplate(OrderData trade, string comment);
+        public static SetTradeDelegateTemplate SetTradeDelegates;
+
+        public delegate void SetPriceDelegateTemplate(PriceData price);
+        public static SetPriceDelegateTemplate SetPriceDelegates;
+
+        public delegate void SetPositionOrderDelegateTemplate(List<PositionData> posOrders);
+        public static SetPositionOrderDelegateTemplate SetPositionOrderDelegates;
+
+        public delegate void SetPositionDelegateTemplate(List<PositionData> posPositions);
+        public static SetPositionDelegateTemplate SetPositionDelegates;
+
+        public delegate void SetChannelCloseDelegateTemplate(string message);
+        public static SetChannelCloseDelegateTemplate SetChannelCloseDelegates;
+
+        public delegate void SetChannelStatusDelegateTemplate(bool connect);
+        public static SetChannelStatusDelegateTemplate SetChannelStatusDelegates;
+    }
+}
---
+++ MQLibrary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MQPanel.Control;
+
+namespace MQPanel
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Tester());
+        }
+    }
+}
